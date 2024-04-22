3d05700ff1179f19f031f3b0b24da88718a81eca
Carlos <carloscga97@gmail.com>
2024-04-22 20:02:57 +0200

    add addTask use case

---
+++ app/src/main/java/com/cazulabs/todoapp/addtasks/domain/AddTaskUseCase.kt
@@ -0,0 +1,12 @@
+package com.cazulabs.todoapp.addtasks.domain
+
+import com.cazulabs.todoapp.addtasks.data.TaskRepository
+import com.cazulabs.todoapp.addtasks.ui.model.TaskModel
+import javax.inject.Inject
+
+class AddTaskUseCase @Inject constructor(private val taskRepository: TaskRepository) {
+
+    suspend operator fun invoke(taskModel: TaskModel) {
+        taskRepository.addTask(taskModel)
+    }
+}
