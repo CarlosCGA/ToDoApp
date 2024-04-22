64b8f5885676682843f0ea2d7730d9608788ff9b
Carlos <carloscga97@gmail.com>
2024-04-22 19:06:29 +0200

    add repository

---
+++ app/src/main/java/com/cazulabs/todoapp/addtasks/data/TaskRepository.kt
@@ -0,0 +1,19 @@
+package com.cazulabs.todoapp.addtasks.data
+
+import com.cazulabs.todoapp.addtasks.ui.model.TaskModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TaskRepository @Inject constructor(private val taskDAO: TaskDAO) {
+
+    val tasks: Flow<List<TaskModel>> =
+        taskDAO.getTasks().map { items -> items.map { TaskModel(it.id, it.task, it.isDone) } }
+
+    suspend fun addTask(taskModel: TaskModel) {
+        taskDAO.addTask(TaskEntity(taskModel.id, taskModel.task, taskModel.isDone))
+    }
+}
+
