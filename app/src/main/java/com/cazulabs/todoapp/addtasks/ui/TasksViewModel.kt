002a8d457204b5b3605fa692a93611096e17a7a4
Carlos <carloscga97@gmail.com>
2024-04-22 20:59:02 +0200

    new tasks now added to room database

--- app/src/main/java/com/cazulabs/todoapp/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/cazulabs/todoapp/addtasks/ui/TasksViewModel.kt
@@ -4,10 +4,27 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cazulabs.todoapp.addtasks.domain.AddTaskUseCase
+import com.cazulabs.todoapp.addtasks.domain.GetTasksUseCase
+import com.cazulabs.todoapp.addtasks.ui.TasksUiState.*
 import com.cazulabs.todoapp.addtasks.ui.model.TaskModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class TasksViewModel @Inject constructor() : ViewModel() {
+class TasksViewModel @Inject constructor(
+    val getTasksUseCase: GetTasksUseCase,
+    private val addTaskUseCase: AddTaskUseCase
+) : ViewModel() {
+
+    val uiState: StateFlow<TasksUiState> = getTasksUseCase().map(::Success)
+        .catch { Error(it) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
 
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
@@ -26,6 +43,10 @@ class TasksViewModel @Inject constructor() : ViewModel() {
     fun onTaskAdded(task: String) {
         onDialogClose()
         _tasks.add(TaskModel(task = task))
+
+        viewModelScope.launch {
+            addTaskUseCase.invoke(TaskModel(task = task))
+        }
     }
 
     fun onCheckBoxSelected(taskModel: TaskModel) {
